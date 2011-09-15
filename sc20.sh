827d23c77f297c24a863acc90a8d5a8ea7aa3251
Rushyang Darji <rushyangdarji@gmail.com>
2011-09-15 11:08:44 +0530

    edited ‘sc20.sh’

--- sc20.sh
+++ sc20.sh
@@ -4,10 +4,10 @@ if [ "$2" = '+' ]; then
 elif [ "$2" = '-' ]; then
 	sub=`expr $1 - $3`
 	echo "The result is: $sub"
-elif [ "$2" = '\*' ]; then
-	mul=`expr $1 \* $3`
-	echo "The result is: $mul"
 elif [ "$2" = '/' ]; then
 	div=`expr $1 / $3`
 	echo "The result is: $div"
+else [ "$2" = '*' ]
+	mul=`expr $1 \* $3`
+	echo "The result is: $mul"
 fi
