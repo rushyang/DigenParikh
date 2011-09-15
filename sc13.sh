a551fa0dacca030571de9db48c157119f0ff7560
Rushyang Darji <rushyangdarji@gmail.com>
2011-09-15 11:09:34 +0530

    edited ‘sc13.sh’

--- sc13.sh
+++ sc13.sh
@@ -5,7 +5,7 @@ read -p "Enter the username: " myuser
 finaluser=`who | awk '{print $1}' | grep "^$myuser$" | head -n1`
 #who prints all users currently logged in into the server.
 # awk '{print $1}' prints the 1st column from the output of "who"
-# grep will try to fetch the $myuser from output.. ^ means MUST be started from the given word.. and $ means MUST be ended from the given word.. Overall exactly THAT word should be exist in ONE particular line
+# grep will try to fetch the $myuser from output.. ^ means MUST be started from the given word.. and $ means MUST be ended from the given word.. Overall, exactly THAT word should be existing in ONE particular line
 # if there are multiple result from grep then head will turn into just 1 line
 # if the $myuser is currently logged in, it will successfully fetched into with the same name into the new variable $finaluser
 if [ "$myuser" = "$finaluser" ]; then
